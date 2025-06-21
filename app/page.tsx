a9697079382b25750de62dd4ed997008a3059237
tpryuki <reptpr0314@gmail.com>
2025-06-22 06:26:33 +0900

    ヘッダー作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <h1 className="font-bold text-3xl underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <header className="text-center">
+          <h1 className="mb-4 font-bold text-3xl ">リノセウス最適解計算機</h1>
+          <p className="text-gray-600">
+            シャドウバース リノセウスの最大ダメージ計算ツール
+          </p>
+        </header>
+
+        {/* <Calculator/> */}
+      </div>
+    </div>
+  );
 }
