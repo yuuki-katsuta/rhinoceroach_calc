bfe281814a51206f21f6edd01e825e78497d78af
tpryuki <reptpr0314@gmail.com>
2025-06-23 02:10:42 +0900

    レイアウト修正

--- app/(calculator)/_components/Hint.tsx
+++ app/(calculator)/_components/Hint.tsx
@@ -1,6 +1,6 @@
 export const Hint = () => {
   return (
-    <div className="rounded-lg border border-yellow-200 bg-yellow-50 p-4">
+    <div className="mt-5 rounded-lg border border-yellow-200 bg-yellow-50 p-4">
       <p className="text-sm text-yellow-800">
         <strong>xxxxx</strong>
       </p>
