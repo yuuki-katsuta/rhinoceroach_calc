7f195feed409b1e66bcb34b9053d266a2b46755c
tpryuki <reptpr0314@gmail.com>
2025-06-22 21:53:43 +0900

    レイアウト作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
+import { Calculator } from "@/components/Calculator";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 py-8">
       <div className="container mx-auto px-4">
         <header className="text-center">
-          <h1 className="mb-4 font-bold text-3xl ">リノセウス最適解計算機</h1>
-          <p className="text-gray-600">
-            シャドウバース リノセウスの最大ダメージ計算ツール
-          </p>
+          <h1 className="mb-4 font-bold text-3xl ">リノ算</h1>
+          <p className="text-gray-600">最大ダメージ計算ツール</p>
         </header>
 
-        {/* <Calculator/> */}
+        <Calculator />
       </div>
     </div>
   );
